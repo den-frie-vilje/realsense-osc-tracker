b1a5028c9d8c8bd2f5362218dcd399a83c296b05
Ole Kristensen <ole@kristensen.name>
2018-11-26 14:13:10 +0100

    + realsense intitialisation

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,6 +3,86 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    trackingMesh.setMode(OF_PRIMITIVE_POINTS);
+
+    rs2::config cfg;
+    
+    cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_ANY, 60);
+    
+    try {
+        
+        selection = pipe.start(cfg);
+        
+        // Find first depth sensor (devices can have zero or more then one)
+        auto depth_sensor = selection.get_device().first<rs2::depth_sensor>();
+        
+        if (depth_sensor.supports(RS2_OPTION_EMITTER_ENABLED))
+        {
+            depth_sensor.set_option(RS2_OPTION_EMITTER_ENABLED, 1.f); // Enable emitter
+        }
+        if (depth_sensor.supports(RS2_OPTION_ENABLE_AUTO_EXPOSURE))
+        {
+            depth_sensor.set_option(RS2_OPTION_ENABLE_AUTO_EXPOSURE, 1.f); // Enable autoexposure
+        }
+        
+        /* manual exposure options
+         
+         if (depth_sensor.supports(RS2_OPTION_GAIN))
+         {
+         depth_sensor.set_option(RS2_OPTION_GAIN, 32.f);
+         }
+         if (depth_sensor.supports(RS2_OPTION_EXPOSURE))
+         {
+         depth_sensor.set_option(RS2_OPTION_EXPOSURE, 4000.f);
+         }
+         */
+        
+        if (depth_sensor.supports(RS2_OPTION_LASER_POWER))
+        {
+            // Query min and max values:
+            auto range = depth_sensor.get_option_range(RS2_OPTION_LASER_POWER);
+            depth_sensor.set_option(RS2_OPTION_LASER_POWER, range.max); // Set max power
+        }
+        
+        auto scale =  depth_sensor.get_depth_scale();
+        
+        auto stream = pipe.get_active_profile().get_stream(RS2_STREAM_DEPTH);
+        if (auto video_stream = stream.as<rs2::video_stream_profile>())
+        {
+            try
+            {
+                //If the stream is indeed a video stream, we can now simply call get_intrinsics()
+                intrinsics = video_stream.get_intrinsics();
+                
+                auto principal_point = std::make_pair(intrinsics.ppx, intrinsics.ppy);
+                auto focal_length = std::make_pair(intrinsics.fx, intrinsics.fy);
+                rs2_distortion model = intrinsics.model;
+                /*
+                 std::cout << "Principal Point         : " << principal_point.first << ", " << principal_point.second << std::endl;
+                 std::cout << "Focal Length            : " << focal_length.first << ", " << focal_length.second << std::endl;
+                 std::cout << "Distortion Model        : " << model << std::endl;
+                 std::cout << "Distortion Coefficients : [" << intrinsics.coeffs[0] << "," << intrinsics.coeffs[1] << "," <<
+                 intrinsics.coeffs[2] << "," << intrinsics.coeffs[3] << "," << intrinsics.coeffs[4] << "]" << std::endl;
+                 */
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << "Failed to get intrinsics for the given stream. " << e.what() << std::endl;
+            }
+        }
+        
+        dec_filter.set_option(RS2_OPTION_FILTER_MAGNITUDE, 2.0);
+        spat_filter.set_option(RS2_OPTION_FILTER_SMOOTH_ALPHA, 0.95f);
+        temp_filter.set_option(RS2_OPTION_FILTER_SMOOTH_ALPHA, 0.1f);
+        temp_filter.set_option(RS2_OPTION_FILTER_SMOOTH_DELTA, 65.0f);
+        temp_filter.set_option(RS2_OPTION_HOLES_FILL, 7);
+
+        
+        
+    } catch (exception e){
+        ofLogError("SETUP", "No realsense camera found");
+    }
+    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ofMain.h"
+#include <librealsense2/rs.hpp>
+
 
 class ofApp : public ofBaseApp{
 
@@ -21,4 +23,29 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+    // TRACKING
+    
+    rs2::pipeline pipe;
+    rs2::device device;
+    rs2::pipeline_profile selection;
+    rs2::colorizer color_map;
+    rs2::frame colored_depth;
+    rs2::frame colored_filtered;
+    rs2_intrinsics intrinsics;
+    
+    rs2::decimation_filter dec_filter;
+    rs2::spatial_filter spat_filter;
+    rs2::temporal_filter temp_filter;
+    
+    rs2::points points;
+    rs2::pointcloud pc;
+    
+    ofMesh trackingMesh;
+    
+    
+    // ofCamera trackingCamera;
+    
+    // ofxCv::KalmanPosition trackingKalman;
+    // MeshTracker tracker;
+    
 };
